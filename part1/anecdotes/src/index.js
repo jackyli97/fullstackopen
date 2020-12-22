1ad8b85f3ec810dc01a75078fb9ab4d76de1b59a
jackyli97 <xwlh808@gmail.com>
2020-12-22 13:00:22 -0800

    Complete anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,35 +1,92 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const randomIdx = () => {
+  return Math.floor(Math.random() * anecdotes.length)
+}
+
+const Header = ({title}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
 const TextDisplay = (props) => {
   return (
-    <div>
-      {props.anecdotes[props.selected]}
-    </div>
+    <>
+      <div>{anecdotes[props.selected]}</div>
+      <div>
+        <VotesDisplay voteCount={props.votes[props.selected]}/>
+      </div>
+    </>
   )  
 }
 
-const NextButton = (props) => {
+const NextButton = ({setSelected}) => {
   return (
-    <button onClick={props.handleClick}>Next Anecdote</button>
+    <button onClick={() => setSelected(randomIdx)}>Next Anecdote</button>
   )  
+}
+
+const VotesDisplay = (props) => {
+  return (
+    <div>has {props.voteCount} votes</div>
+  )
+}
+
+const handleVote = (selected, votes, votesSetter, mostVotes, mostVotesSetter) => () => {
+  votesSetter(votes);
+  if (votes[selected] >= mostVotes["count"]) {
+    mostVotesSetter({"idx": selected, "count": votes[selected]});
+  }
+}
+
+const VotesButton = (props) => {
+  let votesCopy = Array.from(props.votes);
+  votesCopy[props.selected] += 1;
+  return(
+    <button onClick={handleVote(props.selected, votesCopy, props.setVotes, props.mostVotes, props.setMostVotes)}>vote</button>
+  )
+}
+
+const MostVotes = (props) => {
+  let mostPopularAnecdote = props.mostVotes["idx"] === null ? "" : anecdotes[props.mostVotes["idx"]];
+  return(
+    <div>{mostPopularAnecdote}</div>
+  )
+}
+
+const OfTheDay = (props) => {
+  return (
+    <div>
+      <Header title={props.title}/>
+      <TextDisplay votes={props.votes} selected={props.selected} />
+      <VotesButton selected={props.selected} votes={props.votes} setVotes={props.setVotes} setMostVotes={props.setMostVotes} mostVotes={props.mostVotes}/>
+      <NextButton setSelected={props.setSelected} />
+    </div>
+  )
+}
+
+const WithMostVotes = (props) => {
+  return(
+    <div>
+      <Header title={props.title}/>
+      <MostVotes votes={props.votes} mostVotes={props.mostVotes}/>
+    </div>
+  )
 }
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
-
-  const handleClick = (idx) => () => {
-    setSelected(idx)
-  }
-
-  const randomIdx = () => {
-    return Math.floor(Math.random()*anecdotes.length)
-  }
+  const [votes, setVotes] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0));
+  const [mostVotes, setMostVotes] = useState({"idx": null, "count": 0});
+  const ofTheDay = "Anecdote of the day";
+  const withMostVotes = "Anecdote with most votes";
 
   return (
     <div>
-      <TextDisplay anecdotes={props.anecdotes} selected={selected}/>
-      <NextButton handleClick={handleClick(randomIdx)}/>
+      <OfTheDay title={ofTheDay} votes={votes} setVotes={setVotes} selected={selected} setSelected={setSelected} mostVotes={mostVotes} setMostVotes={setMostVotes}/>
+      <WithMostVotes title={withMostVotes} votes={votes} mostVotes={mostVotes} setMostVotes={setMostVotes}/>
     </div>
   )
 }
@@ -44,7 +101,7 @@ const anecdotes = [
 ]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App/>,
   document.getElementById('root')
 )
 
